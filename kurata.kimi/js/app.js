7d847158dd121f98d1d9cc3c648e924fced16a61
kimikurata <kimitomodachi@hotmail.com>
2021-10-07 11:44:33 -0700

    functional user login + modals + drawer

---
+++ kurata.kimi/js/app.js
@@ -0,0 +1,41 @@
+
+// DOCUMENT READY
+$(()=>{
+	checkUserId();
+   // Event Delegation
+   $(document)
+
+   //FORM SUBMITS
+   .on("submit","#signin-form",function(e) {
+      e.preventDefault();
+      checkSigninForm();
+   })
+
+   // ANCHOR CLICKS
+   .on("click",".js-logout",function(e) {
+      e.preventDefault();
+      sessionStorage.removeItem("userId");
+      checkUserId();
+   })
+
+
+
+
+
+
+    .on("click","[data-activate]",function(e){
+      e.preventDefault();
+      let target = $(this).data("activate");
+      $(target).addClass("active");
+    })
+    .on("click","[data-deactivate]",function(e){
+      e.preventDefault();
+      let target = $(this).data("deactivate");
+      $(target).removeClass("active");
+    })
+    .on("click","[data-toggle]",function(e){
+      e.preventDefault();
+      let target = $(this).data("toggle");
+      $(target).toggleClass("active");
+    })
+});
---
+++ kurata.kimi/js/signin.js
@@ -0,0 +1,31 @@
+const checkSigninForm = () => {
+   let user = $("#signin-username").val();
+   let pass = $("#signin-password").val();
+
+   console.log(user,pass);
+   if(user == 'user' && pass == 'pass') {
+      // logged in
+      console.log("success");
+      sessionStorage.userId = 3;
+       $("#signin-form")[0].reset();
+   } else {
+      // not logged in
+      console.log("failure")
+      sessionStorage.removeItem('userId')
+   }
+   checkUserId();
+}
+
+const checkUserId = () => {
+   let p = ['#page-signin','#page-signup',''];
+
+   if(sessionStorage.userId === undefined ){
+      //Not Logged in
+      if(!p.some(o=>window.location.hash===o))
+         $.mobile.navigate("#page-signin");
+   }else{
+      //Logged in
+      if(p.some(o=>window.location.hash===o))
+         $.mobile.navigate("#page-map");   
+   }
+}

11e7e588c620a26ae9f134ce7099e69a957ccf08
kimikurata <kimitomodachi@hotmail.com>
2021-10-13 14:16:28 -0700

    new add-location forms, new map drawer and delete confirmation popup, shados on navbar

--- kurata.kimi/js/app.js
+++ kurata.kimi/js/app.js
@@ -1,41 +1,58 @@
 
 // DOCUMENT READY
 $(()=>{
+
+
+
+  // CHECK DEVICE WIDH
+  var deviceWidth = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+  console.log(deviceWidth);
+  
+  if (deviceWidth%2 == 0) {
+    $(".spacer-container").addClass("spacer-even-width");
+    console.log("even width");
+  }else{
+    $(".spacer-container").addClass("spacer-odd-width");
+    console.log("odd width");
+
+  }
+
+
+
 	checkUserId();
    // Event Delegation
    $(document)
 
    //FORM SUBMITS
    .on("submit","#signin-form",function(e) {
-      e.preventDefault();
-      checkSigninForm();
-   })
+    e.preventDefault();
+    checkSigninForm();
+  })
 
    // ANCHOR CLICKS
    .on("click",".js-logout",function(e) {
-      e.preventDefault();
-      sessionStorage.removeItem("userId");
-      checkUserId();
-   })
+    e.preventDefault();
+    sessionStorage.removeItem("userId");
+    checkUserId();
+  })
 
 
 
 
 
-
-    .on("click","[data-activate]",function(e){
-      e.preventDefault();
-      let target = $(this).data("activate");
-      $(target).addClass("active");
-    })
-    .on("click","[data-deactivate]",function(e){
-      e.preventDefault();
-      let target = $(this).data("deactivate");
-      $(target).removeClass("active");
-    })
-    .on("click","[data-toggle]",function(e){
-      e.preventDefault();
-      let target = $(this).data("toggle");
-      $(target).toggleClass("active");
-    })
-});
+   .on("click","[data-activate]",function(e){
+    e.preventDefault();
+    let target = $(this).data("activate");
+    $(target).addClass("active");
+  })
+   .on("click","[data-deactivate]",function(e){
+    e.preventDefault();
+    let target = $(this).data("deactivate");
+    $(target).removeClass("active");
+  })
+   .on("click","[data-toggle]",function(e){
+    e.preventDefault();
+    let target = $(this).data("toggle");
+    $(target).toggleClass("active");
+  })
+ });
